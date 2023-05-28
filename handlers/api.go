f6b3f0dec44f4bab689ac0b04f87753aa8847034
warutphi <warutphi@ais.co.th>
2023-05-29 02:58:53 +0700

    remove code not use

--- handlers/api.go
+++ handlers/api.go
@@ -17,22 +17,14 @@ func Setup(ctx context.Context, cfg *viper.Viper, router *gin.Engine, mongoInter
 		c.String(http.StatusOK, "service start")
 	})
 	interviewsHandler := NewInterviewsHandler(ctx, cfg, mongoInterviewsClient)
-	//authHandler := NewAuthHandler(ctx, cfg, mongoUsersClient)
-
-	// router.POST("/signin", authHandler.SignIn)
 	router.GET("/interviews", interviewsHandler.ListInterviews)
 	router.GET("/interviews/:id", interviewsHandler.GetInterview)
-
-	// API private endpoints
-	authorised := router.Group("/")
-	//authorised.Use(m.AuthCookieMiddleware())
-	// authorised.POST("/signout", authHandler.SignOut)
-	authorised.POST("/interviews", interviewsHandler.NewInterview)
-	authorised.PUT("/interviews/:id", interviewsHandler.UpdateInterview)
-	authorised.DELETE("/interviews/:id", interviewsHandler.DeleteInterview)
-	authorised.POST("/comment/:id", interviewsHandler.AddComment)
-	authorised.PUT("/comment/:idInterview/:idComment", interviewsHandler.UpdateComment)
-	authorised.DELETE("/comment/:idInterview/:idComment", interviewsHandler.DeleteComment)
+	router.POST("/interviews", interviewsHandler.NewInterview)
+	router.PUT("/interviews/:id", interviewsHandler.UpdateInterview)
+	router.DELETE("/interviews/:id", interviewsHandler.DeleteInterview)
+	router.POST("/comment/:id", interviewsHandler.AddComment)
+	router.PUT("/comment/:idInterview/:idComment", interviewsHandler.UpdateComment)
+	router.DELETE("/comment/:idInterview/:idComment", interviewsHandler.DeleteComment)
 
 	return router
 }
